2abda3dc9e857270d6cdfa9ec9af1913bf9250dc
Fredrik Savje <fred_gh@savje.com>
2019-10-13 23:41:42 -0700

    CV

--- cv/gen_cv.py
+++ cv/gen_cv.py
@@ -24,6 +24,10 @@ def r_space(input):
 def escape_latex(input):
     return input.replace('&', '\\&')
 
+def remove_first_word(input):
+    first, _, rest = input.partition(" ")
+    return rest or first
+
 with open('../data/cv.json') as cv_file:
     cv_data = json.load(cv_file)
 
@@ -35,6 +39,7 @@ env.filters.update({
     'lr_space' : lr_space,
     'l_space' : l_space,
     'r_space' : r_space,
+    'remove_first_word' : remove_first_word,
     'escape_latex' : escape_latex
 })
 env.get_template('cv_template.tex').stream(cv_data).dump('fredrik-savje-cv.tex')
